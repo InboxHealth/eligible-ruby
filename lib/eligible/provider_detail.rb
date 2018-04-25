6b111c832741feabef37f75be137e4a7d6381774
Pavana Teja T <pavana@eligibleapi.com>
2018-04-25 13:33:26 +0530

    ProviderDetail endpoint renamed by ProviderModel

--- lib/eligible/provider_detail.rb
+++
@@ -1,7 +0,0 @@
-module Eligible
-  class ProviderDetail < APIResource
-    def self.get(params, api_key = nil)
-      send_request(:get, '/provider_details.json', api_key, params)
-    end
-  end
-end
