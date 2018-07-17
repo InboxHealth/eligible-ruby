09e8a909d4fc1421b607464e082b57a13ed9e753
Pavana Teja T <pavana@eligible.com>
2018-07-17 23:26:06 +0530

    Risk assessments endpoint support added

---
+++ spec/lib/risk_assessment_spec.rb
@@ -0,0 +1,22 @@
+describe 'RiskAssessment' do
+  let(:params) { { test: true } }
+  let(:api_key) { 'xyz' }
+  let(:response) { { success: true } }
+  before(:each) do
+    allow(Eligible::Util).to receive(:convert_to_eligible_object).with(response, api_key).and_return('success')
+  end
+
+  describe '.criteria' do
+    it 'should call Eligible.request with proper url' do
+      allow(Eligible).to receive(:request).with(:get, '/risk_assessments/criteria.json', api_key, params).and_return([response, api_key])
+      expect(Eligible::RiskAssessment.criteria(params, api_key)).to eq 'success'
+    end
+  end
+
+  describe '.cost_estimate' do
+    it 'should call Eligible.request with proper url' do
+      allow(Eligible).to receive(:request).with(:get, '/risk_assessments/cost_estimates.json', api_key, params).and_return([response, api_key])
+      expect(Eligible::RiskAssessment.cost_estimate(params, api_key)).to eq 'success'
+    end
+  end
+end
