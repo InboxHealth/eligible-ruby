d59db04d6de3f160adf3f58cd88826bbb821ba50
Pavana Teja T <pavana@eligible.com>
2020-04-01 18:40:08 +0530

    fixed codacy

--- lib/eligible/v1_0/insurance_company.rb
+++ lib/eligible/v1_0/insurance_company.rb
@@ -4,11 +4,11 @@ module Eligible
   class InsuranceCompany < RestAPIBase
     ENDPOINT_NAME = 'insurance_companies'.freeze
 
-    def self.create(params, opts = {})
+    def self.create(_params, _opts = {})
       fail NotImplementedError, "Not an allowed operation for this endpoint"
     end
 
-    def self.update(params, opts = {})
+    def self.update(_params, _opts = {})
       fail NotImplementedError, "Not an allowed operation for this endpoint"
     end
   end
