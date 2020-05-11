933017710feb657c73fd5fa476c99cef2b8b99b9
Rodrigo Dominguez <rorra@rorra.com.ar>
2020-05-11 10:12:51 -0300

    Adding action summary for enrollments

--- lib/eligible/v1_0/enrollment.rb
+++ lib/eligible/v1_0/enrollment.rb
@@ -4,6 +4,10 @@ module Eligible
   module V1_0
     class Enrollment < RestAPIBase
       ENDPOINT_NAME = 'enrollments'.freeze
+
+      def self.summary(params, opts = {})
+        send_request :get, "/#{ENDPOINT_NAME}/summary", rest_api_params(params), opts
+      end
     end
   end
 end
